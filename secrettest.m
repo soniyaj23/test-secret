bdbfcaf0ad177a4ee47877e7cfb28cdf87079f72
soniyaj23 <soniyajain0532@gmail.com>
2025-06-06 12:41:24 +0530

    Update secrettest.m

--- secrettest.m
+++ secrettest.m
@@ -2,7 +2,7 @@
 function secrettest
 sprintf("Listing all env variables")
 system('env')
-secretValue = getenv("MATLAB_API_KEY");
+secretValue = getenv("API_KEY");
 % GitHub actions actively prevents logging secret values 
 fprintf('Secret value has length: %d\n', length(secretValue));
 fprintf('First 5 characters of secret: %s...\n', secretValue(1:min(5, end))); % Example: print first 5 chars
