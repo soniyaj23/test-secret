c3c4ca40b0e046a440d7fcb6af268c2ebbbe02f3
soniyaj23 <soniyajain0532@gmail.com>
2026-07-29 21:09:31 +0530

    Update secrettest.m
    Checking prefdir in CICD

--- secrettest.m
+++ secrettest.m
@@ -6,6 +6,10 @@ secretValue = getenv("MY_API_KEY");
 % GitHub actions actively prevents logging secret values 
 fprintf('Secret value has length: %d\n', length(secretValue));
 fprintf('First 5 characters of secret: %s...\n', secretValue(1:min(5, end))); % Example: print first 5 chars
+
+% Checking prefdir in CICD
+fprintf('prefdir = %s\n', prefdir);
+
 % Try to access list of Secrets
 sprintf("Listing all secrets")
 try
