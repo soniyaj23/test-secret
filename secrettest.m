a64155f6171590a1760d4dcf7cb632a72b251faf
soniyaj23 <soniyajain0532@gmail.com>
2026-07-29 22:03:39 +0530

    Update secrettest.m
    Checking value of MATLAB_UNATTENDED_TEST_ENVIRONMENT env var in CICD.

--- secrettest.m
+++ secrettest.m
@@ -8,7 +8,10 @@ fprintf('Secret value has length: %d\n', length(secretValue));
 fprintf('First 5 characters of secret: %s...\n', secretValue(1:min(5, end))); % Example: print first 5 chars
 
 % Checking prefdir in CICD
-fprintf('prefdir = %s\n', prefdir);
+fprintf('prefdir = %s\n\n', prefdir);
+
+% Checking the value of MATLAB_UNATTENDED_TEST_ENVIRONMENT env var
+fprintf('MATLAB_UNATTENDED_TEST_ENVIRONMENT = %d\n', getenv('MATLAB_UNATTENDED_TEST_ENVIRONMENT'));
 
 % Try to access list of Secrets
 sprintf("Listing all secrets")
