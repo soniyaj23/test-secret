5544003eb103e2ff3c3dc61fa88635be735a63ac
soniyaj23 <soniyajain0532@gmail.com>
2026-08-05 12:43:01 +0530

    Check if MATLAB settings persist

--- secrettest.m
+++ secrettest.m
@@ -13,6 +13,15 @@ fprintf('prefdir = %s\n\n', prefdir);
 % Checking the value of MATLAB_UNATTENDED_TEST_ENVIRONMENT env var
 fprintf('MATLAB_UNATTENDED_TEST_ENVIRONMENT = %d\n', getenv('MATLAB_UNATTENDED_TEST_ENVIRONMENT'));
 
+% Checking if MATLAB settings persist
+fprintf('\nshort value of pi = );
+disp(pi);
+s = settings();
+s.matlab.commandwindow.NumericFormat.PersonalValue = 'long';
+fprintf('\nlong value of pi = );
+disp(pi);
+fprintf('\n');
+
 % Try to access list of Secrets
 sprintf("Listing all secrets")
 try
