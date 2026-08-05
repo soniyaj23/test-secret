af03fb2416c87f9bf6c1576301c118e68a3cf023
soniyaj23 <soniyajain0532@gmail.com>
2026-08-05 15:27:04 +0530

    Update setPersonalValue.m

--- setPersonalValue.m
+++ setPersonalValue.m
@@ -18,9 +18,8 @@ function setPersonalValue
   disp(pi);
   fprintf('\n');
 
-  if(numel(dir(prefdir)) > numFiles) {
+  if numel(dir(prefdir)) > numFiles
     disp('Files in prefdir: ');
     disp(dir(prefdir));
-  }
-
+  end
 end
