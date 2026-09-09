6561990bf3d5597ed1b1d1c7c9d1ea7716336ec6
soniyaj23 <soniyajain0532@gmail.com>
2026-09-09 11:38:07 +0530

    Update duckdb.m

--- duckdb.m
+++ duckdb.m
@@ -1,8 +1,16 @@
 function conn = duckdb(filePath,options)
-  arguments
-       filePath (1,1) string = ""
-       options.EncryptionKeyID secretID;
-  end
-  encKey = options.EncryptionKeyID.getSecret();
-  conn = "Report generated successfully!";
+arguments
+    filePath (1,1) string = ""
+    options.EncryptionKeyID secretID
+end
+
+try
+    encKey = options.EncryptionKeyID.getSecret();
+catch ME
+    throwAsCaller(MException( ...
+        "duckdb:VaultUnavailable", ...
+        ME.message));
+end
+
+conn = [];
 end
